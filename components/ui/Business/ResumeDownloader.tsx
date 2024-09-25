c6c1338d3d646718385a6439805fc074f3cfddd8
EPAM\Sanket_Joshi <sanket_joshi@epam.com>
2024-09-25 19:53:55 +0530

    Adding fix

--- components/ui/Business/ResumeDownloader.tsx
+++ components/ui/Business/ResumeDownloader.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { DownloadIcon, FileTextIcon } from "@radix-ui/react-icons";
 import { useFileDownload } from "@/hooks/use-file-download";
-import { useEffect } from "react";
 
 export const ResumeDownloader = () => {
   const { t } = useTranslation("home");
