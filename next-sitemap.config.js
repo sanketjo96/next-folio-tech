fe163c00c096a68dc1a3c165e76f46dc4726ea21
EPAM\Sanket_Joshi <sanket_joshi@epam.com>
2024-10-07 10:58:24 +0530

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   siteUrl: 'https://www.techsanket.in', // Base URL of your site
   generateRobotsTxt: true, // Automatically generate robots.txt file
-  sitemapSize: 7000, // Number of URLs per sitemap file
 };
