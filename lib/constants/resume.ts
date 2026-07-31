764d61966fad4874a2be823d57fc65d489307c6c
sanketjo96 <sanketjoshi96@gmail.com>
2026-07-31 11:08:50 +0000

    Fix resume PDF download by updating stale Google Drive link
    The old Drive file ID was returning 404 during build, causing the
    getStaticProps resume download to silently fail on every build.

--- lib/constants/resume.ts
+++ lib/constants/resume.ts
@@ -1,5 +1,5 @@
 // The URL of the PDF file you want to download
-export const resumeURL = 'https://drive.google.com/uc?export=download&id=1uzadlIy4kqjwy6wsZcQdWBUyiBIXvlkQ';
+export const resumeURL = 'https://drive.google.com/uc?export=download&id=1l4JQquH0-0nTMyoVgs5jJbOfC-z1pGTU';
 
 // The path where you want to save the downloaded PDF
 export const resumePath = './public/downloads/Sanket_FE_JS.pdf';
